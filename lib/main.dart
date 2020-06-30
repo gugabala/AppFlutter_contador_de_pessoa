d69b6e11cb85b993e5cfb52b49d299263390b636
gugabala <gugabala@gmail.com>
2020-06-30 13:53:41 -0300

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/caverna.jpg",
+          fit: BoxFit.cover,
+          height: 1000,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: 0",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Text(
-            "Pode Entrar!",
-            style: TextStyle(
-                color: Colors.blue,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0),
-          )
-        ],
-      )));
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                          fontSize: 40.00,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        debugPrint("+1");
+                      }),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                          fontSize: 40.00,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        debugPrint("-1");
+                      }),
+                ),
+              ],
+            ),
+            Text(
+              "Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
 }
